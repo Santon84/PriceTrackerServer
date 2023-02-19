618878a412c57fbbe6d974cf20685da43a958744
Anton <santon84@gmail.com>
2023-02-19 13:55:54 +0300

    node

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app") ;
-const { getFirestore} = require('firebase/firestore') ;
+const { initializeFirestore } = require('firebase/firestore') ;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+// firebase.firestore().settings({ experimentalForceLongPolling: true });
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+const db = initializeFirestore (app, { experimentalForceLongPolling: true })
 module.exports = { db }
